4a0e5195fe48615cc687352610b8daaa8c2d70d5
Nadia Paola Garcia <ngarcia@tacitknowledge.com>
2015-02-13 20:37:23 -0600

    DOM Test

---
+++ DOMTest/src/dom/test/Employee.java
@@ -0,0 +1,12 @@
+package dom.test;
+
+public class Employee {
+	String id;
+    String firstName;
+	String lastName;
+	String location;
+	@Override
+	public String toString() {
+	    return firstName+" "+lastName+"("+id+")"+location;
+	}
+}
